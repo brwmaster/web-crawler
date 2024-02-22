0f9118e50eaf2ef68d885bdf4760d77e3e92a09e
Edwin Boon <edwin.boon@eo.nl>
2024-02-22 14:54:36 +0100

    Added cli functionality

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+const { exit, argv } = require('process');
+
+function main() {
+    if(argv.length > 3) {
+        console.error('To many arguments')
+        exit(1)
+    }
+
+    if(argv.length < 3) {
+        console.error('To few arguments')
+        exit(1)
+    }
+
+    const baseUrl = argv[2]
+    
+    console.log('-------------------------------------')
+    console.log(`----- start crawling ${baseUrl} -----`)
+    console.log('-------------------------------------')
+
+};
+
+main();
